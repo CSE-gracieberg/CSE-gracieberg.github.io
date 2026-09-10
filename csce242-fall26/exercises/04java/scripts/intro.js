687370fab58e36bf929dc5c075d36332826a5c47
CSE-gracieberg <bergmang@email.sc.edu>
2026-09-10 12:06:47 -0400

    link click

--- csce242-fall26/exercises/04java/scripts/intro.js
+++ csce242-fall26/exercises/04java/scripts/intro.js
@@ -2,3 +2,14 @@ document.getElementById("btn-show-message").onclick = (e) => {
     document.getElementById("p-message").innerHTML ="Hello World";
     e.target.innerHTML="DONE!";
 }
+document.getElementById("link").onclick = (e) => {
+    console.log("link clicked");
+}
+
+//styles when the link is clicked
+document.getElementById("link").onclick = (e) => {
+    e.preventDefault();//dont go to link destination
+    e.target.classList.add("cool-link");
+}
+
+//ball animatoin
